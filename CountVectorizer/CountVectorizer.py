7a5be94553a8b988551fbc9983aa367e554d37aa
Margarita <75544820+MSUdalova@users.noreply.github.com>
2021-10-27 12:56:44 +0300

    Add files via upload

--- CountVectorizer/CountVectorizer.py
+++ CountVectorizer/CountVectorizer.py
@@ -9,6 +9,8 @@ class CountVectorizer:
         self.features = []
 
     def fit_transform(self, corpus: list):
+        """Обучается на корпусе из текстовых документов
+         и возвращает матрицу числа вхождений слов из словаря."""
         lower_split_corpus = [list(map(lambda x: x.lower(),
                                        text.split())) for text in corpus]
         self.features = list(set(chain.from_iterable(lower_split_corpus)))
@@ -21,7 +23,7 @@ class CountVectorizer:
         matrix = []
 
         for text in lower_split_corpus:
-            str_in_matrix = [0] * len(self.features)
+            str_in_matrix = [0 for _ in range(len(self.features))]
             for word in text:
                 str_in_matrix[word_to_index[word]] += 1
             matrix.append(str_in_matrix)
@@ -29,17 +31,19 @@ class CountVectorizer:
         return matrix
 
     def get_feature_names(self):
+        """Возвращает список всех встечающихся слов в корпусе текстов."""
         return self.features
 
 
-corpus = [
-    'Crock Pot Pasta Never boil pasta again',
-    'Pasta Pomodoro Fresh ingredients Parmesan to taste'
-]
+if __name__ == '__main__':
+    corpus = [
+        'Crock Pot Pasta Never boil pasta again',
+        'Pasta Pomodoro Fresh ingredients Parmesan to taste'
+    ]
 
-vectorizer = CountVectorizer()
-count_matrix = vectorizer.fit_transform(corpus)
-print(vectorizer.get_feature_names())
-print(count_matrix)
-print(vectorizer.__doc__)
+    vectorizer = CountVectorizer()
+    count_matrix = vectorizer.fit_transform(corpus)
+    print(vectorizer.get_feature_names())
+    print(count_matrix)
+    print(vectorizer.__doc__)
 
